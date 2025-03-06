2d68303f9ecb83ff3fd3b3cd2b0c1b860c1b67ae
Nanda <nandabahtiar1@gmail.com>
2025-03-06 16:07:51 +0700

    java d2

--- src/ExampleLoop.java
+++ src/ExampleLoop.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ExampleLoop {
     public static void main(String[] args) {
         /**
@@ -5,11 +7,49 @@ public class ExampleLoop {
          * 1 for
          * for inisialisasi; kondidi; increment/decrement
          * 2 while
+         * while(condition){   }
          * 3 do-while
+         * do{
+         * statement} while(kondisi)
          * */
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hello World " + i);
-        }
+//        for (int i= 0; i < 10; i++) {
+//            System.out.println("Hello World " + i);
+//
+//        }
+//        int num = 4;
+//        while(num < 10) {
+//            System.out.println("Hello World " + num);
+//            num++;
+//        }
+//
+//        do {
+//            System.out.println("Hello World " + num);
+//            num++;
+//        }while (num < 10);
+
+        Scanner input = new Scanner(System.in);
+
+        String nama ;
+        // inputan 4 karakter
+//        do {
+//            System.out.print("Masukkan Nama:");
+//            nama = input.nextLine();
+//        }while (nama.length() < 4);
+//        System.out.println(nama);
+
+
+        // game tebak angka
+        //program tenak angka 1 - 10
+        String angka ;
+        do {
+            System.out.print("tebak angka:");
+            angka = input.nextLine();
+            if (angka.isEmpty()){
+                System.out.println("pilihan kosong");
+               break;
+            }
+        }while (Integer.parseInt(angka) != 7 );
+        System.out.println(angka + " ye benar");
     }
 }
